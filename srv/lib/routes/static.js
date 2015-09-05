5fea7388ba3a46e3039c5a81f71eb5bf34d7650a
cdaringe <cdieringer@mrn.org>
2015-09-05 15:50:19 -0700

    simplify static route

--- srv/lib/routes/static.js
+++ srv/lib/routes/static.js
@@ -6,28 +6,11 @@ module.exports.register = function(server, options, next) {
 
     server.route({
         method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-            console.dir(path.join(process.cwd(), '../', 'index.html'));
-            return reply.file(path.join(process.cwd(), '../', 'index.html'));
-        }
-    });
-
-    // server.route({
-    //     method: 'GET',
-    //     path: '/{filename}',
-    //     handler: function (request, reply) {
-    //         console.dir(request.url);
-    //         return reply.file(path.join('../', request.params.filename));
-    //     }
-    // });
-    //
-    server.route({
-        method: 'GET',
         path: '/{param*}',
         handler: {
             directory: {
-                path: path.join(process.cwd(), '../')
+                path: path.join(process.cwd(), '../'),
+                // index: ['index.html']
             }
         }
     });
