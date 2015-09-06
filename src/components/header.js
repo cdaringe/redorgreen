e4a4563a94fa94db6c72bc01312ed174a2046212
cdaringe <cdieringer@mrn.org>
2015-09-05 23:31:48 -0700

    safari now 40% works :/

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default class Header extends React.Component {
     render() {
         return (
-            <div id="rog_banner" className="banner">
-                <div id="rog_title">
+            <div id="rog_banner" className="banner vertical-align">
+                <div>
                     <span className="title title-red">Red</span>
                     <span id="or" className="title">or</span>
                     <span className="title title-green">Green</span>
