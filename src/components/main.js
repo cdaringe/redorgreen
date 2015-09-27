8fed9f480ed308bce4836e98ad2c37985e45ce36
cdaringe <cdieringer@mrn.org>
2015-09-27 16:01:00 -0700

    refactor dumb footer

--- src/components/main.js
+++ src/components/main.js
@@ -7,17 +7,32 @@ import Zia from './zia'
 import ModalStats from './modal-stats'
 import assign from 'lodash.assign'
 
+const isXsViewPort = () => Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 768;
+
 export default class Main extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            statsUp: false
+        };
+        this.handleResize = this.handleResize.bind(this)
+    }
+    componentWillMount() {
+        window.addEventListener("resize", this.handleResize);
+        this.setState({ isXsViewPort: isXsViewPort() });
+    }
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.handleResize);
     }
     componentDidMount() {
         window.notificationSystem = this.refs.notificationSystem;
     }
-    setStatsUp(up) {
+    handleResize() {
+        this.setState({ isXsViewPort: isXsViewPort() });
+    }
+    setStatsUp() {
         let nstate = {};
-        assign(nstate, this.state, {statsUp: up});
+        assign(nstate, this.state, {statsUp: !this.state.statsUp});
         this.setState(nstate);
     }
     render() {
@@ -25,31 +40,39 @@ export default class Main extends React.Component {
         if (this.state.statsUp) {
             modal = <ModalStats setStatsUp={this.setStatsUp.bind(this)} />
         }
+        const footer = (
+            <footer onClick={this.setStatsUp.bind(this)}>
+                <Zia />
+                <small>made with love by <a href="https://github.com/cdaringe/" target="_blank">cdaringe</a></small>
+            </footer>
+        );
+        const xsFooter = this.state.isXsViewPort ? footer : null;
+        const normalFooter = this.state.isXsViewPort ? null : footer;
+
         return (
-            <div className="vbox viewport">
-                <NotificationSystem ref="notificationSystem" />
-                <header>
-                    <Header />
-                </header>
-                <section className="hbox space-between">
-                    <Chiles />
-                </section>
-                <footer>
-                    <div>
-                        <Zia setStatsUp={this.setStatsUp.bind(this)} />
-                    </div>
-                    <small>made with love by <a href="https://github.com/cdaringe/" target="_blank">cdaringe</a></small>
-                </footer>
-                {modal}
+            <div>
+                <div className="vbox viewport">
+                    <NotificationSystem ref="notificationSystem" />
+                    <header>
+                        <Header />
+                    </header>
+                    <section className="hbox space-between">
+                        <Chiles />
+                    </section>
+                    {normalFooter}
+                </div>
+                <div>
+                    {xsFooter}
+                    {modal}
+                </div>
             </div>
         )
     }
-}
-
+};
 app.votePort = 8081;
 
 window.dummyFetch = function() {
-    return fetch({method: 'post', url: 'http://localhost:' + app.votePort})
+    return window.fetch({method: 'post', url: 'http://localhost:' + app.votePort})
     .then(function() { console.log(arguments); })
     .catch(function() { console.log(arguments); });
 }
