a0a96a042620d787cdf5159d56dfe06bd9f8c718
cdaringe <christopher.dieringer@gmail.com>
2017-01-29 18:28:57 -0800

    :art: make huge displays not so crappy

--- packages/ui/src/components/header.js
+++ packages/ui/src/components/header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default class Header extends React.Component {
   render () {
     return (
-      <header {...this.props} style={{padding: '10px 0 5px 0'}}>
+      <header {...this.props}>
         <div className='flex-row-grow'>
           <span className='title title-red'>Red</span>
           <span id='or' className='title'>or</span>
